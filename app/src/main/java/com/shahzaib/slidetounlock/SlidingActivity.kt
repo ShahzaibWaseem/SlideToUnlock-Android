1d9fa82f9a048d3f28d4f0b1d733be79261b2524
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2020-09-17 10:35:43 +0500

    Viewing the Slider (Seek Bar) in the activity

--- app/src/main/java/com/shahzaib/slidetounlock/SlidingActivity.kt
+++ app/src/main/java/com/shahzaib/slidetounlock/SlidingActivity.kt
@@ -1,2 +1,11 @@
 package com.shahzaib.slidetounlock
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SlidingActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sliding_activity)
+    }
+}
